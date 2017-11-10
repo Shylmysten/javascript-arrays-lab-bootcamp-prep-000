4ae2d501a7dafe897a5de091496e57afb0e6a369
Tony Boswell <tboswell@kc.rr.com>
2017-11-10 17:02:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield"]
-// creates a function that appends an element to the end of an arrays
+// creates a function that appends an element to the end of the kittens array
 // destructive
 
 function destructivelyAppendKitten(a) {
   kittens.push(a);
   return kittens;
+}
+
+// creates a function that prepends an element to the beginning the kittens
+// array destructive
+
+function destructivelyPrependKitten(a) {
+  kittens.unshift(a);
+  return kittens;  
 }
