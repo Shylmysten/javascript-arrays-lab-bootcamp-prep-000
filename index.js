2277219181e5474aec4fb45f041a01b9c75034f1
Tony Boswell <tboswell@kc.rr.com>
2017-11-10 16:41:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = "I don't do much."
 // creates a function that appends an element to the end of an arrays
 // destructive
 
-function destructivelyAppendKitten(kitten) {
-  kitten.push();
+function destructivelyAppendKitten(name) {
+  name.push();
   return;
 }
