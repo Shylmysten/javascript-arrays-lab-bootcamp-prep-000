a0984d01fe843093ffa2b2bf2d5ce3fa18cdbd8f
Tony Boswell <tboswell@kc.rr.com>
2017-11-10 17:05:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield"]
 // creates a function that appends an element to the end of the kittens array
-// destructive
+// destructive example
 
 function destructivelyAppendKitten(a) {
   kittens.push(a);
@@ -10,9 +10,17 @@ function destructivelyAppendKitten(a) {
 }
 
 // creates a function that prepends an element to the beginning the kittens
-// array destructive
+// array destructive example
 
 function destructivelyPrependKitten(a) {
   kittens.unshift(a);
+  return kittens;
+}
+
+// creates a function that removes the last element from the kittens array
+// destructive example
+
+function destructivelyRemoveLastKitten(a) {
+  kittens.pop(a);
   return kittens;
 }
