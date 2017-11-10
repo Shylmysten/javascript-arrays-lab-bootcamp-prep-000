1790019097afc9936d5d239add133b1382eab329
Tony Boswell <tboswell@kc.rr.com>
2017-11-10 17:13:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ function destructivelyAppendKitten(a) {
 
 // creates a function that prepends an element to the beginning the kittens
 // array destructive example
-
 function destructivelyPrependKitten(a) {
   kittens.unshift(a);
   return kittens;
@@ -19,8 +18,22 @@ function destructivelyPrependKitten(a) {
 
 // creates a function that removes the last element from the kittens array
 // destructive example
-
 function destructivelyRemoveLastKitten(a) {
   kittens.pop(a);
   return kittens;
 }
+
+// creates a function that removes the First element from the kittens array
+// destructive example
+function destructivelyRemoveFirstKitten(a) {
+  kittens.shift(a);
+  return kittens;
+}
+
+//  creates a function that appends an element to the kittens array and returns
+// a new array  non-destructive/non mutating example
+function appendKitten(a) {
+  var kittens = [kittens..., a]
+  return kittens;
+}
+
